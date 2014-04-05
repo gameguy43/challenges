dc3d2f53d42aeade37ab228d90a3fbd234cd7ccb
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-04 23:33:18 -0700

    removed strategy comments

--- corresponding_parenthesis.rb
+++ corresponding_parenthesis.rb
@@ -1,38 +1,24 @@
 # given a string and the position of an opening parenthesis, find the corresponding closing parenthesis index
 # last updated April 4 2014
 
-test_string = "multip(le(pare)nt(he)s)is"
-
 def find_closing_parenthesis(string, index)
 
-  closing_index = nil
-  i = index + 1
-
-  # set a counter to 0
   depth = 0
+  i = index + 1
+  closing_index = nil
 
-  # go through each character after string[index]
   while closing_index == nil
-
-    # if a beginning parenthesis is encountered, increase counter by 1
     if string[i] == '('
       depth += 1
-
-    # if an end parenthesis is encountered and counter is 0, return index
     elsif ( string[i] == ')' && depth == 0)
       closing_index = i
-
-    # else decrease counter by 1 and keeping going
     elsif string[i] == ')'
       depth -= 1
     end
-
     i += 1
-
   end
 
   return closing_index
-
 end
 
 # edge cases:
