9bd6e0c842b5bb7845d5fc82848c4a780dc0c635
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-04 22:53:39 -0700

    optimized if statement check in push method

--- stack_class.rb
+++ stack_class.rb
@@ -1,6 +1,6 @@
 # an extended stack class with a get_largest method that returns the largest element in the stack
 # push, pop and get_largest methods must have a run time of O(1)
-# last updated April 4 2014
+# completed April 4 2014
 
 class Stack
 
@@ -24,7 +24,7 @@ class Stack
   def push(value)
     @values << value
 
-    if @largest.nil? || value >= @largest
+    if value >= @largest || @largest.nil?
       @largest = value
       @previous_largests << value
     end
