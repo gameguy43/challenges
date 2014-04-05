cf88e750a38d939a54858e8f1fe8edc1778c659e
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-04 23:12:27 -0700

    initial commit

---
+++ corresponding_parenthesis.rb
@@ -0,0 +1,8 @@
+# given a string and the position of an opening parenthesis, find the corresponding closing parenthesis
+# last updated April 4 2014
+
+test_string = "multip(le(pare)nt(he)s)is"
+
+def find_closing_parenthesis(string)
+
+end
