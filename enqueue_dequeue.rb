98b0cb215d61d52279357758634ccd6c6f20847d
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-04 22:59:28 -0700

    initial commit

---
+++ enqueue_dequeue.rb
@@ -0,0 +1,18 @@
+# implements a queue and optimizes the time cost of calling enqueue and dequeue
+# last updated April 4 2014
+
+class Queue
+
+  def initialize
+    #
+  end
+
+  def enqueue
+    #
+  end
+
+  def dequeue
+    #
+  end
+
+end
