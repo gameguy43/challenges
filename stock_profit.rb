49d7f9e01fa583e67684278d140704794151797a
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-06 12:33:40 -0700

    made initial variables more readible, renamed k and v to time and price

--- stock_profit.rb
+++ stock_profit.rb
@@ -1,39 +1,31 @@
 # given a hash of elapsed minutes and stock prices, find the best times to have bought and sold stock
-# last update April 6 2014
-
-time_and_price = { 0 => 100, 10 => 105, 20 => 100, 30 => 100, 40 => 100, 50 => 100 }
+# completed update April 6 2014
 
 def find_times(hash)
 
-  # keep track of lowest price so far
   lowest_price = hash.values[0]
-
-  # keep track of greatest profit, time to buy, and time to sell
-  max_profit = 0
-  buy_time = hash.keys[0]
+  buy_time     = hash.keys[0]
   new_buy_time = hash.keys[0]
-  sell_time = hash.keys[0]
+  sell_time    = hash.keys[0]
+  max_profit   = 0
 
-  # iterate through hash and find profit from buying at lowest price so far
-  hash.each do |k, v|
+  hash.each do |time, price|
 
-    if v < lowest_price
-      lowest_price = v
-      new_buy_time = k
+    if price < lowest_price
+      lowest_price = price
+      new_buy_time = time
     end
 
-    # update greatest profit if profit from current price is greater    
-    current_profit = v - lowest_price
+    current_profit = price - lowest_price
     
     if current_profit > max_profit
       max_profit = current_profit
-      sell_time = k
+      sell_time = time
       buy_time = new_buy_time
     end
 
   end
 
-  # return times from greatest profit
   return "Buy at #{buy_time} min, sell at #{sell_time} min"
 
 end
