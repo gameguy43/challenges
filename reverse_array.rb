424d67cb0e16c2fb89d61d859faef7569b45fe87
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-06 13:00:17 -0700

    removed test array

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,8 +1,6 @@
 # reverses an array of characters without storing a new array in memory
 # completed April 4 2014
 
-array = ['a', 'b', 'd', 'e']
-
 def reverse_array(array)
   i = array.length - 1
   until i < array.length / 2
