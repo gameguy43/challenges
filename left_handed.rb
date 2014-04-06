ed29560c53bb1ba1c9b428ddd1bd2bdfddc5703f
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-06 12:59:02 -0700

    converted tabs to spaces

--- left_handed.rb
+++ left_handed.rb
@@ -2,10 +2,10 @@
 # completed January 4 2014
 
 def left_handed?(word)
-	left_letters = ['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b']
-	word = word.split('')
-	for l in word
-		return false if left_letters.include?(l) == false
-	end
-	return true
+  left_letters = ['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b']
+  word = word.split('')
+  for l in word
+    return false if left_letters.include?(l) == false
+  end
+  return true
 end
