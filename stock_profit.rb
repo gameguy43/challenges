5a6097f2636eafd84765feef3dec1df33227a684
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-06 11:58:22 -0700

    initial commit, with commented strategy

---
+++ stock_profit.rb
@@ -0,0 +1,18 @@
+# given a hash of elapsed minutes and stock prices, find the best times to have bought and sold stock
+# last update April 6 2014
+
+time_and_price = { 0: 100, 10: 120, 20: 140, 30: 110, 40: 100, 50: 130 }
+
+def find_times(hash)
+
+  # keep track of lowest price so far
+
+  # keep track of greatest profit
+
+  # iterate through hash and find profit from buying at lowest price so far
+
+  # update greatest profit if profit from current price is greater
+
+  # return times from greatest profit
+
+end
