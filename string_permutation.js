e1eae4987fe7272db7d4f61bb9691b378e12b6e4
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-04 17:49:09 -0700

    added run time and memory complexity, and commented code

--- string_permutation.js
+++ string_permutation.js
@@ -1,13 +1,20 @@
 // given two strings, return a boolean indicating if they are permutaions of each other
 // completed May 4 2014
 
+// run time complexity   O(n)
+// memory complexity     O(n)
+
 function arePermutations(stringA, stringB) {
+
+  // return false if the strings are different lengths
   if (stringA.length != stringB.length ) return false;
 
+  // set hashes of all characters to their occurences
   var charToOccurA = populateCharOccur(stringA);
   var charToOccurB = populateCharOccur(stringB);
   var chars = Object.keys(charToOccurA);
 
+  // check for differences
   for (var i = 0; i < chars.length; i++) {
     if (charToOccurA[chars[i]] != charToOccurB[chars[i]]) {
       return false;
@@ -16,6 +23,7 @@ function arePermutations(stringA, stringB) {
   return true;
 }
 
+// populate hashes of characters to the number of times they occur
 function populateCharOccur(string) {
   var charToOccur = {};
   for (var i = 0; i < string.length; i++) {
