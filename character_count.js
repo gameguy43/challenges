cf2f2ce872bdc308d0f981002035a1015660513a
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-05 16:16:26 -0700

    added explanation and run time

--- character_count.js
+++ character_count.js
@@ -1,7 +1,12 @@
-var string = "abcbcdhhh";
+// given a string, return another string with each character once preceded
+// by the number of times it occured in the origninal string
+// completed May 5 2014
+
+// run time complexity  O(n)
+// memory complexity    O(n)
 
 function countCharacters(string) {
-  var charCount = "";
+  var charCountStr = "";
   var charToCount = {};
   var character;
 
@@ -15,9 +20,8 @@ function countCharacters(string) {
   }
 
   for (var k in charToCount) {
-    charCount += (charToCount[k] + k);
+    charCountStr += (charToCount[k] + k);
   }
 
-  return charCount;
-
+  return charCountStr;
 }
