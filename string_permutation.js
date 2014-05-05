106248519d77056750b06f9a85dc5378ec077f2c
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-04 17:32:36 -0700

    added populateCharOccurences function and check for equality of character occurences

--- string_permutation.js
+++ string_permutation.js
@@ -7,6 +7,30 @@ var string3 = "cantaloupe";
 function arePermutations(stringA, stringB) {
   if (stringA.length != stringB.length ) return false;
 
+  var charToOccurenceA = populateCharOccurences(stringA);
+  var charToOccurenceB = populateCharOccurences(stringB);
+
+  var characters = Object.keys(charToOccurenceA);
+
+  for (var i = 0; i < characters.length; i++) {
+    if (charToOccurenceA[characters[i]] != charToOccurenceB[characters[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
+function populateCharOccurence(string) {
+  var charToOccurence = {};
+  for (var i = 0; i < string.length; i++) {
+    if ( !charToOccurence[i] ) {
+      charToOccurence[i] = 1;
+    } else {
+      charToOccurence[i] ++;
+    }
+  }
 }
 
 arePermutations(string1, string2);
