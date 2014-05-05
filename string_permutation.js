61ddc022a0a61c587603094917459f786612dc9b
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-04 17:10:04 -0700

    initial commit

---
+++ string_permutation.js
@@ -0,0 +1,13 @@
+// given two strings, return a boolean indicating if they are permutaions of each other
+
+var string1 = "hello";
+var string2 = "lolhe";
+var string3 = "cantaloupe";
+
+function arePermutations(stringA, stringB) {
+  if (stringA.length != stringB.length ) return false;
+
+}
+
+arePermutations(string1, string2);
+arePermutations(string2, string3);
