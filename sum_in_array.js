32c54258468c8551c5bb36efa2e073d18463503a
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-16 22:22:33 -0700

    added and populated valueToIndex hash

--- sum_in_array.js
+++ sum_in_array.js
@@ -1,5 +1,10 @@
 var array = [10, -9, -13, 4, 63, 0, 1]
 
 function sum_in_array(int, array) {
+  var valueToIndex = {};
+
+  for (var i = 0; i < array.length; i++) {
+    valueToIndex[array[i]] = i;
+  }
 
 }
