548eaacfa5283cdde440591d1770f8129a61416b
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-16 22:32:57 -0700

    fixed error in brackets

--- sum_in_array.js
+++ sum_in_array.js
@@ -8,7 +8,7 @@ function sum_in_array(int, array) {
   }
 
   for (var i = 0; i < array.length; i++) {
-    if ( valueToIndex[int - array[i]] && valueToIndex[int - array[i] != i] ) {
+    if ( valueToIndex[int - array[i]] && valueToIndex[int - array[i]] != i ) {
       return true;
     }
   }
