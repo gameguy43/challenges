df9cac6874f0f921b2563a3dedc1af11b024c8ec
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-22 17:19:29 -0700

    added min, max, midValue and nextValue

--- array_rotation_point.js
+++ array_rotation_point.js
@@ -2,7 +2,14 @@ var testArray = [4, 5, 6, 7, 8, 1, 2, 3];
 
 function rotationPoint(array, min, max) {
 
+  min = min || 0;
+  max = max || array.length - 1;
+
   // look at middle value and value at next index
+
+  var midIndex = Math.floor( (max + min) / 2 );
+  var midValue = array[midIndex];
+  var nextValue = array[midIndex + 1];
 
   // if the middle value is greater than the next value, return the next value's index
 
