296083d365d318413d359e25829ca59073b10b6d
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-22 17:09:23 -0700

    commented strategy

--- array_rotation_point.js
+++ array_rotation_point.js
@@ -1,5 +1,17 @@
 var testArray = [4, 5, 6, 7, 8, 1, 2, 3];
 
-function rotationPoint(array) {
-  
+function rotationPoint(array, min, max) {
+
+  // look at middle value and value at next index
+
+  // if the middle value is greater than the next value, return the next value's index
+
+  // else if the middle value is less than the last value, rerun between first and middle
+
+  // else (the middle value is greater than the last value) rerun between middle and last
+
 }
+
+// edge cases
+
+// array is sorted
