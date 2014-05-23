7320b953a5c4d7cd09f8fa53ed1b478155327c61
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-22 17:28:55 -0700

    removed comments

--- array_rotation_point.js
+++ array_rotation_point.js
@@ -7,34 +7,16 @@ function rotationPoint(array, min, max) {
   min = min || 0;
   max = max || array.length - 1;
 
-  // look at middle value and value at next index
-
   var midIndex = Math.floor( (max + min) / 2 );
   var midValue = array[midIndex];
   var nextValue = array[midIndex + 1];
 
-  // if the middle value is greater than the next value, return the next value's index
-
   if ( midValue > nextValue ) {
     return midIndex + 1;
-  }
-
-  // else if the middle value is less than the last value, rerun between first and middle
-
-  else if ( midValue < array[array.length - 1] ) {
+  } else if ( midValue < array[array.length - 1] ) {
     return rotationPoint(array, min, midIndex);
-  }
-
-  // else (the middle value is greater than the last value) rerun between middle and last
-
-  else {
+  } else {
     return rotationPoint(array, midIndex, max);
   }
 
 }
-
-// edge cases
-
-// array is sorted
-
-// array has one element
