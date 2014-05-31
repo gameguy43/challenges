29ca15c8bc5f0dc94c1d12936d3157292ebfa248
Noah Kaplan <noahkaplan4@gmail.com>
2014-05-31 14:13:42 -0700

    started for loop and set up commented jquery and function call lines

--- json_as_html.js
+++ json_as_html.js
@@ -1,9 +1,11 @@
 // write a function that takes a json tree and displays it as
 // nested lists in html
 
-var treeUl = $('#TreeUl'); // <ul id="tree"></ul>
+// var treeUl = $('#TreeUl'); // <ul id="tree"></ul>
 
 function renderJsonTree(json) {
+
+  var treeString = "";
 
   // for each key in the json hash
     // add the key to its parent list
@@ -11,4 +13,12 @@ function renderJsonTree(json) {
       // if it's a string, add it to the key's list
       // if it's a hash, run renderJsonTree(hash)
 
+  for (var key in hash) {
+
+  }
+
+  return treeString
+
 }
+
+// treeUl.append(renderJsonTree(input));
