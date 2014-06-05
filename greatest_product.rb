667e6ae1ec31a3e3415bcf48fd0674db5a2c4bec
Noah Kaplan <noahkaplan4@gmail.com>
2014-06-04 23:32:47 -0700

    added add_greater_number helper method

--- greatest_product.rb
+++ greatest_product.rb
@@ -1,7 +1,18 @@
-integers = [1, 4, 10, 6, 2]
+array = [1, 4, 10, 6, 2, 8, 20]
 
-def greatest_product(array)
+def greatest_product(integers)
 
 end
 
-greatest_product(integers)
+def add_greater_number(three_numbers, int, index=-1)
+  return three_numbers if index < -3
+  if !three_numbers[index] || int > three_numbers[index]
+    replaced_number = three_numbers[index]
+    three_numbers[index] = int
+    return add_greater_number(three_numbers, replaced_number, index - 1)
+  else
+    return add_greater_number(three_numbers, int, index - 1)
+  end
+end
+
+greatest_product(array)
