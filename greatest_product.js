820ccb6fa372b5fbf684638d8aa977734b8af220
Noah Kaplan <noahkaplan4@gmail.com>
2014-06-04 23:09:52 -0700

    converted to ruby

--- greatest_product.js
+++
@@ -1,7 +0,0 @@
-integers = [1, 4, 10, 6, 2]
-
-function greatestProduct(array) {
-  
-}
-
-greatestProduct(integers)
---
+++ greatest_product.rb
@@ -0,0 +1,7 @@
+integers = [1, 4, 10, 6, 2]
+
+def greatest_product(array)
+
+end
+
+greatest_product(integers)
