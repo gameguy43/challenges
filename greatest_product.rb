9b3fe6543265da5a12ec3a1e7c19075eb15a05ce
Noah Kaplan <noahkaplan4@gmail.com>
2014-06-11 10:26:54 -0700

    removed extra comments, added completion date and complexities

--- greatest_product.rb
+++ greatest_product.rb
@@ -1,24 +1,24 @@
 # given an array with at least 3 integers, find the largest product of
 # 3 integers in the array
-# last updated June 4 2014
+# completed June 11 2014
 
-array = [1, 4, 10, 6, 2, 8, 20]
+# run time   O(n)
+# memory     O(1)
 
 def greatest_product(integers)
   three_greatest_numbers = [nil, nil, nil]
-  two_lowest_numbers = [nil, nil]
+  two_lowest_numbers =     [nil, nil]
 
   integers.each do |int|
     insert_number(three_greatest_numbers, int, true)
-    insert_number(two_lowest_numbers, int, false)
+    insert_number(two_lowest_numbers,     int, false)
   end
 
   positive_product = three_greatest_numbers[0] * three_greatest_numbers[1] * three_greatest_numbers[2]
-  negative_product = three_greatest_numbers[0] * two_lowest_numbers[0] * two_lowest_numbers[1]
+  negative_product = three_greatest_numbers[0] * two_lowest_numbers[0]     * two_lowest_numbers[1]
 
   positive_product >= negative_product ? ( return positive_product ) : ( return negative_product )
 end
-
 
 def insert_number(integers, int, greater)
   for i in 0...integers.length
@@ -31,5 +31,3 @@ def insert_number(integers, int, greater)
 end
 
 greatest_product(array)
-
-# does not work with two negative numbers
