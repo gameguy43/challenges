edb172922a6bb928d9054d36105d81e334d09641
Noah Kaplan <noahkaplan4@gmail.com>
2014-06-11 10:40:39 -0700

    renamed variables and added comments for insert_number parameters

--- greatest_product.rb
+++ greatest_product.rb
@@ -5,21 +5,22 @@
 # run time   O(n)
 # memory     O(1)
 
-def greatest_product(integers)
-  three_greatest_numbers = [nil, nil, nil]
-  two_lowest_numbers =     [nil, nil]
+def greatest_product(int_array)
+  three_greatest_ints = [nil, nil, nil]
+  two_lowest_ints =     [nil, nil]
 
-  integers.each do |int|
-    insert_number(three_greatest_numbers, int, true)
-    insert_number(two_lowest_numbers,     int, false)
+  int_array.each do |int|                 # greatest?
+    insert_number(three_greatest_ints, int, true)
+    insert_number(two_lowest_ints,     int, false)
   end
 
-  positive_product = three_greatest_numbers[0] * three_greatest_numbers[1] * three_greatest_numbers[2]
-  negative_product = three_greatest_numbers[0] * two_lowest_numbers[0]     * two_lowest_numbers[1]
+  positive_product = three_greatest_ints[0] * three_greatest_ints[1] * three_greatest_ints[2]
+  negative_product = three_greatest_ints[0] * two_lowest_ints[0]     * two_lowest_ints[1]
 
-  positive_product >= negative_product ? ( return positive_product ) : ( return negative_product )
+  positive_product > negative_product ? ( return positive_product ) : ( return negative_product )
 end
 
+                # array     int  boolean
 def insert_number(integers, int, greater)
   for i in 0...integers.length
     if integers[i].nil? || ( greater && int > integers[i] ) || ( !greater && int < integers[i] )
@@ -27,7 +28,4 @@ def insert_number(integers, int, greater)
       break
     end
   end
-  return integers
 end
-
-greatest_product(array)
