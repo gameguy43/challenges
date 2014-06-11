01d41e0637c8e51f0b085e07b54187aa7a2230b1
Noah Kaplan <noahkaplan4@gmail.com>
2014-06-10 17:02:19 -0700

    dryed up code by combining add_greater_number and add_lower_number into one method

--- greatest_product.rb
+++ greatest_product.rb
@@ -5,25 +5,29 @@
 array = [1, 4, 10, 6, 2, 8, 20]
 
 def greatest_product(integers)
-
   three_greatest_numbers = [nil, nil, nil]
+  two_lowest_numbers = [nil, nil]
 
   integers.each do |int|
-    add_greater_number(three_greatest_numbers, int)
+    insert_number(three_greatest_numbers, int, true)
+    insert_number(two_lowest_numbers, int, false)
   end
 
-  # return three_greatest_numbers.inject { |product, int| product * int }
+  positive_product = three_greatest_numbers[0] * three_greatest_numbers[1] * three_greatest_numbers[2]
+  negative_product = three_greatest_numbers[0] * two_lowest_numbers[0] * two_lowest_numbers[1]
 
+  positive_product >= negative_product ? ( return positive_product ) : ( return negative_product )
 end
 
-def add_greater_number(three_numbers, int)
-  for i in 0..2
-    if three_numbers[i].nil? || int > three_numbers[i]
-      three_numbers.insert(i, int).pop
+
+def insert_number(integers, int, greater)
+  for i in 0...integers.length
+    if integers[i].nil? || ( greater && int > integers[i] ) || ( !greater && int < integers[i] )
+      integers.insert(i, int).pop
       break
     end
   end
-  return three_numbers
+  return integers
 end
 
 greatest_product(array)
